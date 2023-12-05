ea5a98999a7a45dcba1f645053a6fd53a0c92e4f
Minh Trinh <trinh.ducminh1990@gmail.com>
2023-12-05 17:32:28 +0700

    add build configs and script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const commonConfig = require("./webpack.common");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js"
+    // publicPath: "/container/latest/"
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+      }
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
